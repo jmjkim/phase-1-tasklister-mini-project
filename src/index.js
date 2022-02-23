e904033ea3b4372aa6597782c4ec7d33933d2256
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-02-22 19:35:03 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  console.log("DOM is now loaded.")
+  
+  const submitEvent = document.getElementById('create-task-form');
+  submitEvent.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const tasks = document.getElementById('tasks');
+
+    const newTask = document.createElement('li');
+    newTask.textContent = document.getElementById('new-task-description').value;
+    tasks.appendChild(newTask);
+    console.log(newTask.textContent);
+    
+    const btn = document.createElement('button');
+    btn.textContent = 'X'
+    btn.addEventListener('click', () => newTask.remove());
+    newTask.append(btn);
+  })
+})
